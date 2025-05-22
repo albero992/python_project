e89fbdbf9ff584b0125b9aa93658b90912aeddb5
Ricardo Albero Rico <albero992@gmail.com>
2025-05-22 16:43:42 +0200

    Ejercicio de tareas (un TO-DO)

---
+++ ejemplo_listas.py
@@ -0,0 +1,16 @@
+mi_lista = [1,2,3,4,5]
+
+print(f"Elemento popeado: {mi_lista[-1]}")
+
+#LIFO
+print (mi_lista)
+
+mi_lista.insert(0,6)
+
+print("Lista")
+
+print("Lista ordenada " , mi_lista.sort())
+
+reversed(mi_lista)
+
+print(reversed)
